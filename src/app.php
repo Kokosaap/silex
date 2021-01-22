cb2d62af128e643c95f4dfef30a9b35749ce3c59
leanaelle.salmier <leanaelle.salmier@epitech.eu>
2021-01-22 13:21:27 +0100

    first page

--- src/app.php
+++ src/app.php
@@ -1,7 +1,14 @@
 <?php
 
-require __DIR__ . '../vendor/autoload.php';
+use Symfony\Component\HttpFoundation\Response;
+
+require __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
+$app['debug'] = true;
+
+$app->match('/', function (){
+	return new Response('Voici ma premiere page');
+});
 
 $app->run();
